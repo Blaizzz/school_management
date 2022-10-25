8c160faf43ca3a20df44c98f0afe3cb3eddda13d
antoine.goulard <antoine.goulard@yahoo.com>
2022-10-25 10:46:09 +0200

    My first commit

---
+++ src/main/java/efrei/fr/App.java
@@ -0,0 +1,13 @@
+package efrei.fr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
