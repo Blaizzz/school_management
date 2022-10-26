fa152ab65253aef878ef9e693e97c097095ebe8f
mohammadcdl <94165640+mohammadcdl@users.noreply.github.com>
2022-10-26 15:28:48 +0200

    Add files via upload

---
+++ src/main/java/efrei/fr/department.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class department {
+    private double Grade;
+    public double getGrade() {
+        return Grade;
+    }
+
+    public void setGrade(double grade) {
+        Grade = grade;
+    }
+
+    public department(double grade) {
+        Grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "department{" +
+                "Grade=" + Grade +
+                '}';
+    }
+
+
+
+
+}
