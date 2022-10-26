85ec592657f5487ef24d6439e82fa4f2ed2dac4a
ClementZeg <94602259+ClementZeg@users.noreply.github.com>
2022-10-26 15:20:47 +0200

    Add files via upload
    //Class Subject

---
+++ src/main/java/efrei/fr/Subject.java
@@ -0,0 +1,35 @@
+public class Subject {
+    private String subjectCode;
+    private String subjectDescription;
+    private Double grade;
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public void setSubjectCode(String subjectCode) {
+        this.subjectCode = subjectCode;
+    }
+
+    public String getSubjectDescription() {
+        return subjectDescription;
+    }
+
+    public void setSubjectDescription(String subjectDescription) {
+        this.subjectDescription = subjectDescription;
+    }
+
+    public Double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Double grade) {
+        this.grade = grade;
+    }
+
+    public Subject(String subjectCode, String subjectDescription, Double grade) {
+        this.subjectCode = subjectCode;
+        this.subjectDescription = subjectDescription;
+        this.grade = grade;
+    }
+}
